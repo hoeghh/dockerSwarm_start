8627bc0633c805488b4defd2cca196f3fce16d63
hoeghh <heh@praqma.net>
2015-07-16 12:28:53 +0200

    Now using same version of boot2docker as host docker client

--- createSwarm.sh
+++ createSwarm.sh
@@ -3,14 +3,18 @@ clear;
 if [ -f /etc/redhat-release ]; then
         sudo setenforce 0
 fi
+boot2dockerVersion="v"$(docker --version |cut -d"," -f1|cut -d" " -f3)
+#If it fails, use these acording to your local docker client version
+#boot2dockerVersion="v1.5.0"
+#boot2dockerVersion="v1.6.2"
+#boot2dockerVersion="v1.7.1"
 
 SwarmID=`docker run swarm create`
 echo $SwarmID
 
-eval $(docker-machine env --swarm swarm-master)
-
 docker-machine create \
     -d virtualbox \
+    --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/$boot2dockerVersion/boot2docker.iso \
     --swarm \
     --swarm-master \
     --swarm-discovery token://$SwarmID \
@@ -18,6 +22,7 @@ docker-machine create \
 
 docker-machine create \
     -d virtualbox \
+    --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/$boot2dockerVersion/boot2docker.iso \
     --swarm \
     --swarm-discovery token://$SwarmID \
     swarm-node-00
