5794b22a3e077e6f5c1d0df6134a451f0304409e
Henrik René Høegh <hoeghh@localhost.localdomain>
2015-08-06 09:02:23 +0200

    Small changes

--- createSwarm.sh
+++ createSwarm.sh
@@ -3,15 +3,15 @@ clear;
 if [ -f /etc/redhat-release ]; then
         sudo setenforce 0
 fi
-boot2dockerVersion="v"$(docker --version |cut -d"," -f1|cut -d" " -f3)
+#boot2dockerVersion="v"$(docker --version |cut -d"," -f1|cut -d" " -f3)
 #If it fails, use these acording to your local docker client version
 #boot2dockerVersion="v1.5.0"
 #boot2dockerVersion="v1.6.2"
-#boot2dockerVersion="v1.7.1"
+boot2dockerVersion="v1.7.1"
 
 SwarmID=`docker run swarm create`
 echo $SwarmID
-
+isoHome="/home/hoeghh/Development/dockerSwarm_start"
 docker-machine create \
     -d virtualbox \
     --virtualbox-boot2docker-url https://github.com/boot2docker/boot2docker/releases/download/$boot2dockerVersion/boot2docker.iso \
@@ -35,5 +35,5 @@ sleep 15
 
 docker info
 
-docker-compose up
-docker run swarm list token://$SwarmID
+#docker-compose up
+#docker run swarm list token://$SwarmID
